65060c0ffba9e92304f7e8dcd3705f3b57f70141
hugo <149073556+fioranellii@users.noreply.github.com>
2025-12-10 21:47:21 -0300

    Add files via upload

---
+++ adm.sql
@@ -0,0 +1,9 @@
+INSERT INTO FUNCIONARIOS (id_funcionario, nome, cpf, senha, cargo) 
+VALUES 
+(
+    4, 
+    'Amanda Gerente', 
+    '444.444.444-44', 
+    '1524e93096b7f32906b3f9479b1875c786636730591f4864b281b672a95c7329', 
+    'Gerente'
+);
